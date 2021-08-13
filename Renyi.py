7752b53d8df72a9a4ca75c5f2e0b1babe91ce4e3
Bilal Riaz <34689025+Bilal092@users.noreply.github.com>
2021-08-12 22:08:11 -0400

    Add files via upload

---
+++ Renyi.py
@@ -0,0 +1,25 @@
+#%%
+
+import numpy as np
+import scipy as sp 
+
+#%%
+# Renyi Entropy function
+'''
+This script computes Renyi Entropy of a point on probaility Simplex.
+'''
+
+def Entropy(p, alph):
+    assert(np.all(p >= 0)),"Probabilities cannot be neagtive"
+    assert(alph >=0 ), "order cannot be nagative"
+    # Shannon entropy
+    if alpha == 1:
+        return -sum(p*np.log2(p))
+    # Min Entropy
+    elif alpha == np.inf:
+        return -np.log2(np.max(2))  
+    else:
+        return (1/(1-alph)) * np.log2(np.sum(p**alph))
+
+
+# %%
